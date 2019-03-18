74ce21bc0785e81698b5a14134f19251206d06f5
baraniprasathkvk <barani.prasath.kvk@gmail.com>
2019-03-18 23:54:43 +0530

    UI using Bootstrap

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>$100000</h1>
+        <button type="button" className="btn btn-success">Credit 10000</button>
+        <button type="button" className="btn btn-danger">Debit 10000</button>
+        <button type="button" className="btn btn-warning">To Charity</button>
       </div>
     );
   }
