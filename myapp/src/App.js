7bcba0c4f6786de0469e91f9835ad018282a3c89
baraniprasathkvk <barani.prasath.kvk@gmail.com>
2019-03-20 19:46:44 +0530

    Without redux

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,18 +1,47 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class Counter extends React.Component {
+  state = {count: 0};
+
+  credit = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  debit = () => {
+    if (this.state.count > 0) {
+      this.setState({
+      count: this.state.count - 1
+    });
+    }
+    else {
+      alert("Cannot Debit. Your Balance is 0!");
+    }
+  };
+
+  donate = () => {
+    if (this.state.count > 0) {
+      this.setState({
+      count: this.state.count - this.state.count
+    });
+    }
+    else {
+      alert("Cannot Donate. Your Balance is 0!");
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>$100000</h1>
-        <button type="button" className="btn btn-success">Credit 10000</button>
-        <button type="button" className="btn btn-danger">Debit 10000</button>
-        <button type="button" className="btn btn-warning">To Charity</button>
+      <div className="counter">
+        <h1 className="count">{this.state.count}</h1>
+        <button type="button" className="btn btn-success" onClick={this.credit}>Credit 10000</button>
+        <button type="button" className="btn btn-danger" onClick={this.debit}>Debit 10000</button>
+        <button type="button" className="btn btn-warning" onClick={this.donate}>To Charity</button>
       </div>
     );
   }
 }
 
-export default App;
+export default Counter;
