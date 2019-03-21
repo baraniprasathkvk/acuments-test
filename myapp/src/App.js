5e7f7ffe1705ea02d5596e431b2053a431ca5582
baraniprasathkvk <barani.prasath.kvk@gmail.com>
2019-03-22 00:16:37 +0530

    with Redux Thunk (2)

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -29,16 +29,13 @@ class Balance extends React.Component {
         debit.disabled = false;
       }
       }
-      ,1);
+      ,50);
     }
-    // tooltip = () => {
-    //   this.$el = $('#button').tooltip('hide');
-    // }
 
   render() {
     return (
       <div className="balance" onLoad={this.empty()}>
-        <h1 className="count" id="balance" /*onChange={this.checkCount().bind(this)}*/>{this.props.count}</h1>
+        <h1 className="count" id="balance">{this.props.count}</h1>
         <button type="button" className="btn btn-success" onClick={this.credit}>Credit $10000</button>
         <button type="button" id="debit" className="btn btn-danger" onClick={this.debit} data-toggle="tooltip" data-placement="top" title="You cannot reduce the money from 0 balance" >Debit $10000</button>
         <button type="button" id="donate" className="btn btn-warning" onClick={this.donate}>To Charity</button>
--- myapp/src/actions/actions.js
+++ myapp/src/actions/actions.js
@@ -1,13 +1,28 @@
-export const Credit = () => {
+export const CreditAsync = () => {
 	return {type: "CREDIT"};
 };
 
-export const Debit = (count) => {
-	if (count!=0) {
+export const Credit = val => {
+	return dispatch => {
+		setTimeout(() => {
+			dispatch(CreditAsync());
+		},200)
+	}
+}
+
+export const DebitAsync = (count) => {
+	if (count!==0) {
 		return {type: "DEBIT"};
 	}
 };
 
+export const Debit = val => {
+	return dispatch => {
+		setTimeout(() => {
+			dispatch(DebitAsync());
+		},200)
+	}
+}
 export const Donate = () => {
 	return {type: "DONATE"};
 }
